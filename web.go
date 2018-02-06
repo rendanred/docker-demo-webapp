f401fff11b4e2f47945a4e462293693757ce5fc0
rendanred <rendanred@gmail.com>
2018-02-06 21:57:47 +0800

    add print

--- web.go
+++ web.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"fmt"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("/web/static")))
 	http.ListenAndServe(":3000", nil)
+	hostname, err := os.Hostname()
+	if err == nil {
+	    fmt.Println("My hostname is ", hostname)
+	}
 }
